2e11ba716a716a025464d76c574caab1432968d2
Michelle Wang <tianw0420@gmail.com>
2022-03-31 21:48:36 -0700

    added fontawesome icon for tasks

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleMinus } from '@fortawesome/free-solid-svg-icons'
 
-function Overview(props) {
-    const { tasks } = props;  
-    return (
-        <div className='Overview'> 
-            <div className="tasks">
-                {tasks.map((task) => {
-                    return <div key={task.id}>{task.number + ' ' + task.text}</div>
-                })}
-            </div> 
-        </div> 
-    ); 
+class Overview extends React.Component {
+    constructor(props){
+        super(); 
+        //this.state = props; 
+    }
+    render(){
+        const { tasks } = this.props; 
+        return(
+            <div className='Overview'> 
+                <div className="tasks">
+                    {tasks.map((task) => {
+                        return (
+                            <div key={task.id}> 
+                                <div>{task.number + ': ' + task.text}</div>
+                                <FontAwesomeIcon icon={faCircleMinus} /> 
+                            </div> 
+                        )
+                    })}
+                    
+                </div> 
+            </div>
+        ) 
+    }; 
 }
 
 export default Overview; 
